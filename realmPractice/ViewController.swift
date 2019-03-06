f7316741a18e8fd6d3f4ced78c40cad3eceaf8d0
ta_fukase <ta_fukase@pc2013-022.pcp.local>
2019-03-06 11:01:48 +0900

    Initial Commit

---
+++ realmPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realmPractice
+//
+//  Created by ta_fukase on 2019/03/06.
+//  Copyright © 2019 ta_fukase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
